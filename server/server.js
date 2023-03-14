1602eb9b811fbff431ec53fda14b9d1ad16e8d4a
HerviBest <hervipro@gmail.com>
2023-03-14 22:02:35 +0700

    feat(User) : Create server.js index for Express App

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const morgan = require('morgan')
+const connectDB = require('./config/db')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+// Config dotev
+require('dotenv').config({
+    path: './config/config.env'
+})
+
+
+const app = express()
+
+// Connect to database
+connectDB();
+
+// body parser
+app.use(bodyParser.json())
+// Load routes
+const authRouter = require('./routes/auth.route')
+
+
+// Dev Logginf Middleware
+
+
+app.use(cors({
+    origin: process.env.CLIENT_URL
+}))
+app.use(morgan('dev'))
+
+
+
+// Use Routes
+app.use('/api', authRouter)
+
+
+app.use((req, res) => {
+    res.status(404).json({
+        success: false,
+        msg: "Page not founded"
+    })
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
